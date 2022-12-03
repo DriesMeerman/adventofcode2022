d91f7f8de97fc60c17f2d468655544bd4a987f51
Dries Meerman <driesmeerman@gmail.com>
2022-12-03 15:53:33 +0100

    add day three challenge 2

--- day3.js
+++ day3.js
@@ -1,4 +1,4 @@
-import { addNumList, intersect, mapLine, getInputFile } from "./helper.js";
+import { addNumList, intersect, mapLine, getInputFile , splitByLine} from "./helper.js";
 
 const example = 
 `vJrwpWtwJgWrhcsFMMfFFhFp
@@ -38,7 +38,7 @@ function challenge1(input){
         // console.log('bpr', right, bpr);
 
         let both = [...new Set(intersect(bpl, bpr))][0]
-        console.log('both', both);
+        // console.log('both', both);
 
         // console.log('left', bpl);
         // console.log('right', bpr);
@@ -60,10 +60,45 @@ function backpackStringToPrio(txt){
     });
 }
 
+let groupByN = (n, data) => {
+    let result = [];
+    for (let i = 0; i < data.length; i += n) result.push(data.slice(i, i + n));
+    return result;
+  };
+
+function challenge2(input){
+    console.log('Challenge 2');
+
+    let lines = splitByLine(input);
+    let grouped = groupByN(3,lines);
+
+    let nums = grouped.map( (group) => {
+        let result = existsInThree(group)[0];
+        // console.log(result)
+        let prio = getPrio(result);
+        // console.log('zj', prio);
+        return prio
+    })
+
+    console.log(nums);
+    console.log("Total: ", addNumList(nums))
+}
+
+function existsInThree(lists){
+    const [a,b,c] = lists.map(n => n.split(''));
+
+    let both = [...new Set(intersect(a, b))]
+
+    let final = [...new Set(intersect(both, c))];
+    return final;
+
+}
+
 function main(){
-    //let input = example;
+    // let input = example;
     let input = getInputFile('day3.txt');
     challenge1(input);
+    challenge2(input);
 }
 
 main();
