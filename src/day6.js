a508a37609b2debdf89beb995659f5546255ce35
Dries Meerman <driesmeerman@gmail.com>
2022-12-08 11:10:47 +0100

    day 6, challenge 2

--- src/day6.js
+++ src/day6.js
@@ -10,26 +10,38 @@ nppdvjthqldpwncqszvftbrmjlhg`
 function challenge1(input) {
     console.log('Challenge 1')
     let result = mapLine(input, (line) => {
-        let block = [];
-        let current = 0;
-        for (const char of line.split('')){
-            block.push(char);
-            if (block.length == 4){
-                let uniqueCount = new Set(block).size
-                if (uniqueCount == 4){
-                    let streamStart = current + 1;
-                    console.log(`Found target at: ${streamStart} ${block}`);
-                    break;
-                }
-
-                block.shift();
-            }
-            current++
-        }
+        return findDistinctCharacters(line, 4);
     })
+
+    console.log('Start of packets', result)
 }
+
+function findDistinctCharacters(line, charCount){
+    let block = [];
+    let current = 0;
+    for (const char of line.split('')){
+        block.push(char);
+        if (block.length == charCount){
+            let uniqueCount = new Set(block).size
+            if (uniqueCount == charCount){
+                let streamStart = current + 1;
+                console.log(`Found target at: ${streamStart} ${block}`);
+                return streamStart;
+            }
+
+            block.shift();
+        }
+        current++
+    }
+}
+
 function challenge2(input) {
     console.log('Challenge 2')
+    let result = mapLine(input, (line) => {
+        return findDistinctCharacters(line, 14);
+    })
+
+    console.log('Start of packets', result)
 }
 
 function main(){
