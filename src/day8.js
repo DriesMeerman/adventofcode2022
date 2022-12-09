a4d05f8fa88451d3afac5de9f7f7f352f56317ea
Dries Meerman <driesmeerman@gmail.com>
2022-12-09 22:24:58 +0100

    day 8 part 2

--- src/day8.js
+++ src/day8.js
@@ -24,6 +24,36 @@ function isVisible(y,x, matrix){
     return compute(isVisibleTop) || compute(isVisibleRight) || compute(isVisibleBottom) || compute(isVisibleLeft);
 }
 
+function countVisible(y,x, matrix){
+    let yMin = 0;
+    let xMin = 0;
+    let xMax = matrix[0].length - 1;
+    let yMax = matrix.length - 1;
+    let edges = [xMin, xMax, yMin, yMax];
+
+
+
+    const compute = (fn) => fn(x,y,edges,matrix);
+    let top = compute(countVisibleTop);
+    let right = compute(countVisibleRight);
+    let bottom = compute(countVisibleBottom);
+    let left = compute(countVisibleLeft);
+
+
+    let tmp = matrix.map(n => n.slice());
+    tmp[y][x] = String(tmp[y][x])
+//     console.table(tmp);
+
+//     console.log(`[${x},${y}]\n\t[${top}]
+// [${left}]\t\t[${right}]
+// \t[${bottom}]
+//     `)
+
+    let scenicScore = top * right * bottom * left ;
+    // console.log('Scenic', scenicScore)
+    return scenicScore
+}
+
 function isVisibleTop(x,y,edges, matrix) {
     let [xMin, xMax, yMin, yMax] = edges;
     for (let i = y-1; i >= yMin; i--){
@@ -68,6 +98,62 @@ function isVisibleLeft(x,y,edges, matrix){
     return true;
 }
 
+function countVisibleTop(x,y,edges, matrix) {
+    let count = 0;
+    let [xMin, xMax, yMin, yMax] = edges;
+    for (let i = y-1; i >= yMin; i--){
+        count++;
+        if (matrix[y][x] <= matrix[i][x]) {
+            // tree is taller vision is blocked
+            return count;
+        }
+        
+    }
+    return count;
+}
+
+function countVisibleRight(x,y,edges, matrix){
+    let count = 0;
+    let [xMin, xMax, yMin, yMax] = edges;
+    for (let i = x+1; i <= xMax; i++){
+        count++;
+        if (matrix[y][x] <= matrix[y][i]) {
+            // tree is taller vision is blocked
+            return count;
+        }
+        
+    }
+    return count;
+}
+
+function countVisibleBottom(x,y,edges, matrix) {
+    let count = 0;
+    let [xMin, xMax, yMin, yMax] = edges;
+    for (let i = y+1; i <= yMax; i++){
+        count++;
+        if (matrix[y][x] <= matrix[i][x]) {
+            // tree is taller vision is blocked
+            return count;
+        }
+        
+    }
+    return count;
+}
+
+function countVisibleLeft(x,y,edges, matrix){
+    let count = 0;
+    let [xMin, xMax, yMin, yMax] = edges;
+    for (let i = x-1; i >= xMin; i--){
+        count++;
+        if (matrix[y][x] <= matrix[y][i]) {
+            // tree is taller vision is blocked
+            return count;
+        }
+        
+    }
+    return count;
+}
+
 function inputToMatrix(input){
     return mapLine(input, (line) => {
         return line.split('').map(Number);
@@ -96,7 +182,26 @@ function challenge1(input) {
     console.log('Result: ', result);
 }
 function challenge2(input) {
-    console.log('Challenge 2')
+    console.log('Challenge 2');
+    let matrix = inputToMatrix(input);
+
+
+    // let ba = countVisible(1,2,matrix)
+    // let ab = countVisible(3,2,matrix)
+    // console.log('GO', ba, ab)
+
+    let result = -1;
+    matrix.forEach( (row, y) => {
+        row.forEach((tree, x) => {
+            let visibleCount =  countVisible(y, x, matrix);
+            if (visibleCount > result) {
+                result = visibleCount;
+            }
+        });
+    });
+
+    console.log('Result: ', result);
+
 }
 
 function main(){
